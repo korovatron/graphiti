f761228ebc4f0dbecc682766f6df28859a57b8ad
Neil <neil@logan-5.co.uk>
2025-10-14 18:00:20 +0100

    Revert button text back to '+ New Function' (singular)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'graphiti-restore-point-14-10-2025-17:30';
+const CACHE_NAME = 'graphiti-restore-point-14-10-2025-17:31';
 const ASSETS_TO_CACHE = [
     './index.html',
     './main.js',
