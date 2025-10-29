f7bcd1de50f2d4a0ffb545526940a95ffaea5795
Neil <neil@logan-5.co.uk>
2025-10-29 19:18:41 +0000

    Bump service worker cache version

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'graphiti-offline-cache-29-10-2025-13:05';
+const CACHE_NAME = 'graphiti-offline-cache-29-10-2025-13:15';
 const ASSETS_TO_CACHE = [
     './',
     './index.html',
