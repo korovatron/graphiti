2b3e8a884dbc85efd77f9cbc6039078aaca67122
Neil <neil@logan-5.co.uk>
2026-02-02 21:48:02 +0000

    bump version number

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // Graphiti - Mathematical Function Explorer
 // Main application logic with animation loop and state management
 
-const VERSION = '1.0.17';
+const VERSION = '1.0.18';
 
 class Graphiti {
     constructor() {
