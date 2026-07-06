b74c8f7cd3c69beafaa4fc1e330af707d0f2b2ed
Neil <neil@logan-5.co.uk>
2026-07-06 21:59:41 +0100

    Bump app and service worker version to 1.1.22

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // Graphiti - Mathematical Function Explorer
 // Main application logic with animation loop and state management
 
-const VERSION = '1.1.21';
+const VERSION = '1.1.22';
 
 class Graphiti {
     constructor() {
