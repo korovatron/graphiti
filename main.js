c01844e764dfe21061b9e76fe8f7fa04c6b2b0db
Neil <neil@logan-5.co.uk>
2026-07-07 21:57:43 +0100

    Bump app and cache version to 1.1.24

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // Graphiti - Mathematical Function Explorer
 // Main application logic with animation loop and state management
 
-const VERSION = '1.1.23';
+const VERSION = '1.1.24';
 
 class Graphiti {
     constructor() {
