cb501568e67c44090b10499bdf95b93509f50961
Neil <neil@logan-5.co.uk>
2026-07-10 16:25:25 +0100

    Bump app and SW version to 1.1.83

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // Graphiti - Mathematical Function Explorer
 // Main application logic with animation loop and state management
 
-const VERSION = '1.1.82';
+const VERSION = '1.1.83';
 
 class Graphiti {
     constructor() {
