dfe88693159617b0071ccea0527afbfdd25afbc8
Neil <neil@logan-5.co.uk>
2026-07-11 21:37:51 +0100

    Polish toolbar buttons and bump versions to 1.1.101

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // Graphiti - Mathematical Function Explorer
 // Main application logic with animation loop and state management
 
-const VERSION = '1.1.100';
+const VERSION = '1.1.101';
 
 class Graphiti {
     constructor() {
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'graphiti-v1.1.100';
+const CACHE_NAME = 'graphiti-v1.1.101';
 const ASSETS_TO_CACHE = [
     './',
     './index.html',
