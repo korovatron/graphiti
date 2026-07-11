ae688b8173fe02de36dea7565993defe4a7bd603
Neil <neil@logan-5.co.uk>
2026-07-11 22:15:34 +0100

    Make export modal scrollable

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // Graphiti - Mathematical Function Explorer
 // Main application logic with animation loop and state management
 
-const VERSION = '1.1.102';
+const VERSION = '1.1.103';
 
 class Graphiti {
     constructor() {
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'graphiti-v1.1.102';
+const CACHE_NAME = 'graphiti-v1.1.103';
 const ASSETS_TO_CACHE = [
     './',
     './index.html',
