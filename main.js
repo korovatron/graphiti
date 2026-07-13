8684b7929b7f14ce37ab055491a7b381f9471f78
Neil <neil@logan-5.co.uk>
2026-07-13 19:04:38 +0100

    Bump version to 1.1.124

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // Graphiti - Mathematical Function Explorer
 // Main application logic with animation loop and state management
 
-const VERSION = '1.1.123';
+const VERSION = '1.1.124';
 
 class Graphiti {
     constructor() {
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'graphiti-v1.1.123';
+const CACHE_NAME = 'graphiti-v1.1.124';
 const ASSETS_TO_CACHE = [
     './',
     './index.html',
