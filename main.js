193248b838d81d5ecc1db0311adaa86eab93b5f6
Neil <neil@logan-5.co.uk>
2026-07-20 16:06:13 +0100

    Bump app version and fix guide iOS notch colour

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // Graphiti - Mathematical Function Explorer
 // Main application logic with animation loop and state management
 
-const VERSION = '1.2.61';
+const VERSION = '1.2.62';
 
 class Graphiti {
     constructor() {
