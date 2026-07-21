5184be5c0a4b2c77418102199711721c7a0e2c7a
Neil <neil@logan-5.co.uk>
2026-07-21 22:55:39 +0100

    Bump version to 1.2.77

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // Graphiti - Mathematical Function Explorer
 // Main application logic with animation loop and state management
 
-const VERSION = '1.2.76';
+const VERSION = '1.2.77';
 
 class Graphiti {
     constructor() {
