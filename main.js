7fdab9263ebc33cc7cdbdc9b976fd7a9d4bf6727
Neil <neil@logan-5.co.uk>
2026-07-24 13:42:48 +0100

    Fixed broken LD structure in index.html and guide.html. Added missing script tag for main.js in index.html. Updated sw.js to include new caching strategy for assets.

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // Graphiti - Mathematical Function Explorer
 // Main application logic with animation loop and state management
 
-const VERSION = '1.3.13';
+const VERSION = '1.3.14';
 
 class Graphiti {
     constructor() {
