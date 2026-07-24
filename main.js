484fa4d886a9e86c942a8a19e074bd6ac11a582a
Neil <neil@logan-5.co.uk>
2026-07-24 17:41:20 +0100

    Split polar and parametric guide sections

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // Graphiti - Mathematical Function Explorer
 // Main application logic with animation loop and state management
 
-const VERSION = '1.3.18';
+const VERSION = '1.3.19';
 
 class Graphiti {
     constructor() {
