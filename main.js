3fe84d06d656242403e7ee161726313bbfddca4c
Neil <neil@logan-5.co.uk>
2026-07-30 20:05:17 +0100

    u[pdated manifest again]

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // Graphiti - Mathematical Function Explorer
 // Main application logic with animation loop and state management
 
-const VERSION = '1.3.56';
+const VERSION = '1.3.57';
 
 class Graphiti {
     constructor() {
