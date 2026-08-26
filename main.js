915e32b56789c3bd8012a9bb5f61e8ebcc52eba1
Neil <neil@logan-5.co.uk>
2026-08-26 21:46:42 +0100

    v1.3.87: Launch button styled to match guide.html

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // Graphiti - Mathematical Function Explorer
 // Main application logic with animation loop and state management
 
-const VERSION = '1.3.86';
+const VERSION = '1.3.87';
 
 class Graphiti {
     constructor() {
