fb989f54e2ed13aa3a0d2f6e6a4d90e3886493c0
Neil <neil@logan-5.co.uk>
2026-08-26 21:55:07 +0100

    v1.3.88 - reduce launch button padding

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // Graphiti - Mathematical Function Explorer
 // Main application logic with animation loop and state management
 
-const VERSION = '1.3.87';
+const VERSION = '1.3.88';
 
 class Graphiti {
     constructor() {
