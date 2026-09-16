77820ab1d7a31565199bbc6b52feeb27af4a401b
Neil <neil@logan-5.co.uk>
2026-09-16 18:04:21 +0100

    Bumped version to 1.4.45

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // Graphiti - Mathematical Function Explorer
 // Main application logic with animation loop and state management
 
-const VERSION = '1.4.4';
+const VERSION = '1.4.45';
 
 class Graphiti {
     constructor() {
